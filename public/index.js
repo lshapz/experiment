7524b990412e78056f8bd65ff87e5dffaa7cfee0
Laura Shapiro <Laura.Shapiro@eikospartners.com>
2017-08-22 11:08:40 -0400

    snake and youtube dock together neatly

--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-let child, child2
+let child, snake, youtube
 var count = 0
 
 function createWindow (){ 
@@ -11,10 +11,31 @@ function createWindow (){
 
 }
 
-function createAnother(){
+function createYoutube(){
+	// can we have an argument that's a youtube URL or search? 
+	youtube = new windowmanager.Window({url: "embed.html", width: 580, height: 600, frame: false, left: 350, top: 300})
+	console.log(youtube)
+	var srcs = windowmanager.Window.getAll().map(item=>{return {src: item._window.src, window: item}})
+	
+	var snake = srcs.filter(item=>{return item.src}).filter(item=>{return item.src.includes('snake')})[0].window
+	youtube.dock(snake)
 
-	child2 = new windowmanager.Window({url: "embed.html", width: 700, height: 700})
-	console.log(child2)
+/*	child.onReady(()=>{
+		child.focus()
+	})*/
+
+}
+
+
+
+function createSnake(){
+	snake = new windowmanager.Window({url: "snake.html", width: 600, height: 600, frame: false, left: 931, top: 225})
+	console.log(snake)
+	var srcs = windowmanager.Window.getAll().map(item=>{return {src: item._window.src, window: item}})
+	
+	var tube = srcs.filter(item=>{return item.src}).filter(item=>{return item.src.includes('embed')})[0].window
+	snake.dock(tube)
+	// debugger
 /*	child.onReady(()=>{
 		child.focus()
 	})*/
