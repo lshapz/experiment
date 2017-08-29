03d522636adee674066653816300192353f2d9c7
Laura Shapiro <laurashapiro01@gmail.com>
2017-08-29 17:15:04 -0400

    vue component, does not interact with windowmanager yet

---
+++ public/View.vue
@@ -0,0 +1,27 @@
+<template>
+
+</template>
+<script>
+Vue.Component('counter', {
+	name: 'counter',
+	template: '{{number}}<button @click.prevent="addOne">Add One</button><button @click.prevent="minusOne">Minus One</button>',
+	data: function(){
+		return {
+			number: 0
+		}
+	},
+	methods: {
+		addOne: function(){
+			this.number += 1
+		},
+		minusOne: function(){
+			this.number -= 1
+		}
+	}
+})
+
+var view = new Vue({
+	el: "#counter"
+})
+
+</script>
